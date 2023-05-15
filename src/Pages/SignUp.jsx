1fa960e2f78ddd03fcdfd3d6ab02ecb40d782c98
Rabia Tanweer <rabiatanveer27@gmail.com>
2023-05-15 20:29:02 +0500

    Signup form details in localstorage

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from  '../styles/Signup.module.css'
 
+
 export default function SignUp() {
+    const[email, setEmail]= useState("");
+    const[password, setPassword]= useState("");
+    const handleEmailChange= (e)=>{
+        setEmail(e.target.value);
+    };
+    const handlePasswordChange= (e)=>{
+        setPassword(e.target.value);
+    };
+    const handleSubmit= (e)=>{
+        e.preventDefault();
+        localStorage.setItem(email, setEmail)
+        localStorage.setItem(password, setPassword)
+    };
   return (
     <div className={style.main}>
-     <form action="action_page.php" >
+     <form onSubmit={handleSubmit} >
   <div className={style.container}>
     <h1>Sign Up</h1>
     <p>Please fill in this form to create an account.</p>
     <hr />
     <label htmlFor="email"><b>Email</b></label>
-    <input type="text" placeholder="Enter Email" name="email" required />
+    <input type="text" placeholder="Enter Email" name="email" value={email} onChange={handleEmailChange} required />
     <label htmlFor="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required />
+    <input type="password" placeholder="Enter Password" name="psw" value={password} onChange={handlePasswordChange} required />
    
     
     <div className= {style.clearfix}>
