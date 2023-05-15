3e46bef49613a8ca20bfdbf9f9cca81e14e6e6b0
Rabia Tanweer <rabiatanveer27@gmail.com>
2023-05-15 19:55:27 +0500

    next quote button functionality

--- src/Pages/Quote.jsx
+++ src/Pages/Quote.jsx
@@ -1,73 +1,77 @@
-import React, { useEffect, useState } from 'react'
-import style from '../styles/Quote.module.css'
-import axios from 'axios'
+// import React, { useEffect, useState } from 'react'
+// import style from '../styles/Quote.module.css'
+// import axios from 'axios'
 
 
+
+// export default function Quote() {
+//     const [data, setData] = useState({ });
+//     const [quote, setQuote]= useState()
+  
+//     useEffect(()=>{
+//       axios.get("https://type.fit/api/quotes").then(
+//         res=>setData(res.data[Math.floor(Math.random()*res.data.length)]))
+//       }, []
+//     )
+    
+//   return (
+//     <div className={style.main}>
+//     <h1>  Welcome To Random Quotes Generator APP</h1>
+     
+//       <div className={style.card}>
+//         <h2>
+//            " {data.text}"
+//         </h2>
+//         <h5> {data.author}</h5>
+//         <button className= {style.button}> Click for next Quote</button>
+//       </div>
+//     </div>
+//   )
+// }
+
+import React, { useEffect, useState } from 'react';
+import style from '../styles/Quote.module.css';
+import axios from 'axios';
 
 export default function Quote() {
-    const [data, setData] = useState({ });
-    const [quote, setQuote]= useState()
-  
-    useEffect(()=>{
-      axios.get("https://type.fit/api/quotes").then(
-        res=>setData(res.data[Math.floor(Math.random()*res.data.length)]))
-      }, []
-    )
-    
+  const [data, setData] = useState({});
+  const [quote, setQuote] = useState();
+
+  useEffect(() => {
+    // Check if data exists in local storage
+    const storedData = localStorage.getItem('quoteData');
+    if (storedData) {
+      setData(JSON.parse(storedData));
+    } else {
+      fetchData();
+    }
+  }, []);
+
+  const fetchData = () => {
+    axios.get('https://type.fit/api/quotes').then((res) => {
+      const randomIndex = Math.floor(Math.random() * res.data.length);
+      const randomQuote = res.data[randomIndex];
+      setData(randomQuote);
+      localStorage.setItem('quoteData', JSON.stringify(randomQuote));
+    });
+  };
+
+  const handleNextQuote = () => {
+    fetchData();
+  };
+
   return (
     <div className={style.main}>
-    <h1>  Welcome To Random Quotes Generator APP</h1>
-     
+      <h1> Welcome To Random Quotes Generator APP</h1>
+
       <div className={style.card}>
-        <h2>
-           " {data.text}"
-        </h2>
+        <h2>" {data.text}"</h2>
         <h5> {data.author}</h5>
-        <button className= {style.button}> Click for next Quote</button>
+        <button className={style.button} onClick={handleNextQuote}>
+          Click for next Quote
+        </button>
       </div>
     </div>
-  )
+  );
 }
-
-
-// import React, { useEffect } from 'react';
-// import axios from 'axios';
-
-// const Quote = () => {
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await axios.get('https://type.fit/api/quotes');
-//         const data = response.data;
-
-//         // Store data in local storage
-//         localStorage.setItem('quoteData', JSON.stringify(data));
-//       } catch (error) {
-//         console.error('Error fetching data:', error);
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   const getDataFromLocalStorage = () => {
-//     // Retrieve data from local storage
-//     const storedData = localStorage.getItem('quoteData');
-
-//     if (storedData) {
-//       const parsedData = JSON.parse(storedData);
-//       // Use the data as needed
-//       console.log(parsedData);
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <button onClick={getDataFromLocalStorage}>Get Data from Local Storage</button>
-//       <h2>{getDataFromLocalStorage.text} </h2>
-//     </div>
-//   );
-// };
-
-// export default Quote;
 
